2b6345e94b70e348486b951bfb0196c3892b5ae9
Eliza Weisman <hi@hawkweisman.me>
2016-12-10 12:05:11 -0500

    day 4: solve part 2

--- src/Day4.hs
+++ src/Day4.hs
@@ -3,6 +3,7 @@ module Day4( day4 ) where
     import Util
     import Data.Char
     import Data.List
+    import Control.Monad
 
     -- | Parses all of the letters in the name of a room
     nameLetters :: String -> String
@@ -15,15 +16,33 @@ module Day4( day4 ) where
     realChecksum = take 5 . drop 1 . dropWhile (/= '[')
 
     isValid :: String -> Bool
-    isValid s = expectedChecksum s == realChecksum s
+    isValid = liftM2 (==) expectedChecksum realChecksum
 
-    sectorID :: String -> Integer
+    sectorID :: String -> Int
     sectorID = read . filter isNumber
         -- where isNum c = isAlphaNum c && not (isAlpha c)
+
+    caesar :: Int -> String -> String
+    caesar n = fmap shift
+        where shift :: Char -> Char
+              shift c
+                | isAlpha c = remap $ (demap c + n) `mod` 26
+                | c == '-'  = ' '
+                | otherwise = c
+              demap = subtract (ord 'a') . ord
+              remap = chr . (ord 'a' +)
+
+    decrypt :: String -> String
+    decrypt s = caesar (sectorID s) namePart
+        where namePart = takeWhile (/= '[') s
 
     day4 :: IO ()
     -- day4 = undefined
     day4 = do
         input <- getInput "day4"
-        print "Solution 1"
-        print . sum $ (sectorID <$> filter isValid (lines input))
+        let rooms = lines input
+        putStr "Solution 1: "
+        print . sum $ (sectorID <$> filter isValid rooms)
+        putStrLn "Solution 2:"
+        -- print . filter ("north pole" `isPrefixOf`) $ decrypt <$> rooms
+        mapM_ putStrLn $ decrypt <$> rooms
